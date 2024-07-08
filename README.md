86c7571d3983e6be6769cee88b148f3f66d8018b
Jisus <jesusdev2407@gmail.com>
2024-07-08 16:02:19 -0500

    start-proyect

---
+++ README.md
@@ -0,0 +1,3 @@
+# play_safe_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/play_safe_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.play_safe_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
