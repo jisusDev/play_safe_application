2cde46bc2eadbde54dca127643ee302cbe387089
Francisco Colmenarez <156324130+Francisco-V98@users.noreply.github.com>
2024-07-12 15:38:37 -0400

    created button

---
+++ lib/widgets/buttomsheet/ps_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:play_safe_application/config/colors/ps_appcolor.dart';
+
+class PsButton extends StatelessWidget {
+  final String? text;
+  final Function()? onTap;
+  final Color? backgorundColor;
+  const PsButton({
+    super.key,
+    this.text,
+    this.onTap,
+    this.backgorundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: PsAppcolor.primary,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: PsAppcolor.black.withOpacity(0.20),
+              blurRadius: 4,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 40),
+          child: Text(
+            text ?? 'Error',
+            style: const TextStyle(
+                color: PsAppcolor.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
+  }
+}
