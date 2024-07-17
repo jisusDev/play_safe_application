bca0c950a6f59c20ec14fe43cff46caf1af68e5e
Francisco Colmenarez <156324130+Francisco-V98@users.noreply.github.com>
2024-07-17 16:48:06 -0400

    create card end player

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,5 @@
+export 'package:play_safe_application/widgets/card/ps_card_end_player.dart';
+
 export 'package:play_safe_application/widgets/modal/ps_modal.dart';
 
 export 'package:play_safe_application/widgets/buttomsheets/ps_bottomsheet_base.dart';
@@ -8,3 +10,4 @@ export 'package:play_safe_application/widgets/image_and_text/ps_image_and_text.d
 export 'package:play_safe_application/widgets/bottons/ps_button.dart';
 export 'package:play_safe_application/widgets/chip/ps_chip.dart';
 export 'package:play_safe_application/widgets/textfields/ps_textfield.dart';
+
