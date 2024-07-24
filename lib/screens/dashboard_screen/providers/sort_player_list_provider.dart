b5be4caf5aa71655ae3f76ca8cc02df3974af4f5
Francisco Colmenarez <156324130+Francisco-V98@users.noreply.github.com>
2024-07-24 16:40:23 -0400

    add comment in providers

--- lib/screens/dashboard_screen/providers/sort_player_list_provider.dart
+++ lib/screens/dashboard_screen/providers/sort_player_list_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:play_safe_application/domain/entities/player.dart';
 import 'package:play_safe_application/screens/dashboard_screen/providers/providers.dart';
 
+//Sort the list of players by finished ones and completion time
 final sortPlayerListNotifierProvider = Provider<List<Player>>((ref) {
   final players = ref.watch(playerNotifierProvider);
   final now = DateTime.now();
--- lib/screens/dashboard_screen/providers/states_providers.dart
+++ lib/screens/dashboard_screen/providers/states_providers.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// changing selection of bottomsheet buttons
+//position of pressed button in bottomsheet
 final activeButtonProvider = StateProvider<int>((ref) => 0);
 
+//value of time buttons in bottomsheet
 final selectTimeInMinuteProvider = StateProvider<int>((ref) => 5);
 
+//selected time value in seconds in bottomsheet
 final selectTimeInSecondProvider = StateProvider<int>((ref) {
   final int valueInMinutes =
       ref.read(selectTimeInMinuteProvider.notifier).state;
@@ -14,14 +16,15 @@ final selectTimeInSecondProvider = StateProvider<int>((ref) {
   return valueInSeconds;
 });
 
+//value between the start time and the end time
 final updateTimeProvider = StateProvider<DateTime>((ref) {
   final int addMinutes = ref.watch(selectTimeInMinuteProvider);
   DateTime now = DateTime.now();
   return now.add(Duration(minutes: addMinutes));
 });
 
+//TextEditingController of bottomsheet textfields
 final representativeControllerProvider =
     Provider<TextEditingController>((ref) => TextEditingController());
-
 final playerControllerProvider =
     Provider<TextEditingController>((ref) => TextEditingController());
